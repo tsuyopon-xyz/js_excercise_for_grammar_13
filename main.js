fbeb681689833a838f350f912da23fb70fb0ae5b
長塚かおり <nagatsukakaori@nagatsukakaorinoMacBook-Air.local>
2019-03-16 00:01:37 +0900

    first commit

--- main.js
+++ main.js
@@ -17,16 +17,51 @@
  *       - 自作したforEachメソッドを使って実装する
  *         - this経由でforEachメソッドを実行する
  */
-
+const myArray = {
+    array: [1, 2, 3],
+    forEach: function (callback) {
+        for (let i = 0; i < this.array.length; i++) {
+            callback(this.array[i], i);
+        }
+    },
+    map: function (callback) {
+        const mapArray = [];
+        this.forEach((value, index) => {
+            mapArray.push(callback(value, index));
+        });
+        return mapArray;
+    },
+    filter: function (callback) {
+        const filterArray = [];
+        this.forEach((value, index) => {
+            if (callback(value)) {
+                filterArray.push(value);
+            }
+        });
+        return filterArray;
+    }
+}
 
 // ここでmyArray.forEachを実行して、console.logを使って各値とインデックスを出力する
-
-
+console.log('forEach---------------');
+myArray.forEach((value, index) => {
+    console.log(`index : ${index}, value : ${value}`);
+});
 // ここでmyArray.mapを実行したあと、mapの戻り値とmyArray.arrayをconsole.logで出力する
 // mapメソッドで取得する値は、myArray.arrayの各要素をindex倍した値とする。(value * index)
 // mapの戻り値は[0, 2, 6], myArray.arrayの値は[1, 2, 3]であるべき
-
-
+console.log('map---------------');
+const mapValue = myArray.map((value, index) => {
+    return value * index;
+});
+console.log('myArray.arrayは [1, 2, 3] であるべき。 :', myArray.array);
+console.log('mapの戻り値は [0, 2, 6] でありべき。 :', mapValue);
 //ここでfilterを実行したあと、filterの戻り値とmyArray.arrayをconsole.logで出力する
 // filterメソッドで取得する値は、myArray.arrayの奇数だけとする
 // filterの戻り値は[1, 3], myArray.arrayの値は[1, 2, 3]であるべき
+console.log('filter---------------');
+const filterValue = myArray.filter((value) => {
+    return value % 2 === 1;
+});
+console.log('myArray.arrayは [1, 2, 3] であるべき。 :', myArray.array);
+console.log('filterの戻り値は [1, 3] であるべき :', filterValue);
